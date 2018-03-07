dc66e82fc05565046fc18d37215c83afd157edf3
Lorena Reyes <lorena.n.reyes@gmail.com>
2018-03-07 01:34:29 +0000

    first migration

---
+++ postgres/knexfile.js
@@ -0,0 +1,13 @@
+const settings = require("./settings");
+
+module.exports = {
+  development: {
+   client: 'pg',
+  connection: {
+    host: settings.hostname,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database
+    }
+  },
+};
